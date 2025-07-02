bcb20e7885a1b683659f00d7c2aecfc513a33ab2
SomyadeepAdhikari <somyadeepadhikari@gmail.com>
2025-07-02 22:12:23 +0530

    1st commit

---
+++ lib/bloc/bloc_event.dart
@@ -0,0 +1,19 @@
+part of './bloc.dart';
+
+class MessageEvent {}
+
+class DataSent extends MessageEvent {
+  DataSent();
+}
+
+class DataRecieving extends MessageEvent {
+  DataRecieving();
+}
+
+class Pending extends MessageEvent {
+  Pending();
+}
+
+class ImageSelected extends MessageEvent {
+  ImageSelected();
+}
